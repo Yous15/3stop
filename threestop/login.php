fa0fae7a463af5324fb124df911da80a1b567f39
Yous15 <a16josortmar@iam.cat>
2017-01-13 19:32:13 +0100

    update login de prueba
    Añado user de prueba para acceder y algunas redirecciones para hacer bien el login.

--- threestop/login.php
+++ threestop/login.php
@@ -1,6 +1,36 @@
+
 <?php
-$usuari=$_POST['usr'];
-$contrasenya=$_POST['pass'];
-$accedir=$_POST['enviar'];
+  $usuari=$_POST['usr'];
+  $contrasenya=$_POST['pass'];
+
+  $admusr = "admin"; //user de prueba
+  $admpasswd = "123456"; //su pass
+
+  $resultadousr = strcmp($usuari, $admusr);
+  $resultadopass = strcmp($contrasenya, $admpasswd);
+
+  if (isset($_POST['entrar'])){
+
+    if ($resultadousr) {
+
+
+      header('Location: https://www.google.es');
+      //redireccion, cambiar el link por página de error de autenticación
+
+    } else{
+
+      if ($resultadopass){
+
+          header('Location: https://www.google.es');
+
+        //redirección a pagina de error de autenticación
+      } else{
+
+      header('Location: https:/'); //redirección a menú
+
+      }
+    }
+  }
+
 
 ?>
